db25a66ccfb7093dc38e3004635ce6eb30da71b5
Nicolas Marini <nmarini44@gmail.com>
2019-05-31 20:07:49 +0000

    Automatically backed up by Learn

--- app/workers/Leads_worker.rb
+++ app/workers/Leads_worker.rb
@@ -4,6 +4,6 @@ class LeadsWorker
 
   def perform(leads_file)
     CSV.foreach(leads_file, headers: true) do |lead|
-      Customer.create(email:lead[0], first_name: lead[1], last_name: lead[2]))
+      Customer.create(email:lead[0], first_name: lead[1], last_name: lead[2])
   end
 end
